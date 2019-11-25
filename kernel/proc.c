26c6abef45dfcae1b949b1de876ee1060dbe50ef
Ben Kelly <ben@benjii.net>
2019-11-25 20:39:34 +0200

    kernel: Continue implementing process code

--- kernel/proc.c
+++ kernel/proc.c
@@ -1,9 +1,24 @@
 #include <proc.h>
+#include <kernel.h>
 #include <stdint.h>
+#include <mm/pmm.h>
+#include <mm/vmm.h>
 
 static struct proc *proc_table;
 
 static uint16_t next_pid = 1;
+
+static inline void find_next_pid()
+{
+	struct proc *tmp;
+
+	do {
+		if (next_pid++ <= 1)
+			next_pid = 2;
+
+		HASH_FIND_INT(proc_table, &next_pid, tmp);
+	} while (tmp);
+}
 
 static struct proc *proc_alloc()
 {
@@ -16,9 +31,25 @@ static struct proc *proc_alloc()
 	memset(p, 0, sizeof(*p));
 
 	p->state = P_EMBRYO;
-	p->pid = next_pid++;
+	p->pid = next_pid;
+	p->kstack_phys = pmm_alloc();
 
-	HASH_ADD_INT(proc_table, p->pid, p);
+	HASH_ADD_INT(proc_table, pid, p);
+
+	find_next_pid();
 
 	return p;
+}
+
+void proc_userinit()
+{
+	struct proc *init;
+
+	init = proc_alloc();
+
+	init->pgdir_phys = pmm_alloc();
+	if (!init->pgdir_phys)
+		panic("%s: OOM\n", __func__);
+
+	init->pgdir = vmm_create_pd(init->pgdir_phys);
 }
--- kernel/proc.h
+++ kernel/proc.h
@@ -1,6 +1,7 @@
 #ifndef __KERNEL_PROC_H_
 #define __KERNEL_PROC_H_
 
+#include <mm/mmu.h>
 #include <uthash.h>
 
 enum procstate {
@@ -16,6 +17,16 @@ struct proc {
 	enum procstate state;
 	int pid;
 
+	struct pd *pgdir;
+	phys_addr_t pgdir_phys;
+
+	phys_addr_t kstack_phys;
+
 	UT_hash_handle hh;
 };
 
+
+void proc_userinit();
+
+
+#endif
